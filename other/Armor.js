bac88897c98da6b44feef69252cd841969273c99
xpepelok <69628212+xpepelok@users.noreply.github.com>
2021-02-22 19:25:10 +0200

    Add files via upload

--- other/Armor.js
+++ other/Armor.js
@@ -11,25 +11,24 @@
     function drawArmor(slot, x, y) {
         if (armorEnabled) {
 	    RenderHelper.enableGUIStandardItemLighting();
-            let inv = minecraft.getPlayer().getInventory()
-            let item = inv.armorItemInSlot(slot)
-            let amount = item.getCount()
-            let damage = item.getItemDamage()
-            let maxDamage = item.getMaxDamage()
-
-            Draw.renderItemAndEffectIntoGUI(item, x, y + 5)
-            Draw.drawStringWithShadow((damage == maxDamage || damage == 0) ? '' : maxDamage - damage, x + 16, y + 9)
-            Draw.renderItemOverlayIntoGUI(item, x, y + 5, amount == 1 ? '' : amount)
+            let item = Inventory.getStackInSlot(slot)
+            let amount = Inventory.getCount(slot)
+            let damage = Inventory.getDurability(slot)
+            var maxDamage = Inventory.getMaxDamage(slot)
+            
+            Draw.renderItemAndEffectIntoGUI(item, x + 6, y + 5)
+            Draw.drawStringWithShadow(damage == maxDamage ? "" : damage, x + 22, y + 11)
+            Draw.renderItemOverlayIntoGUI(item, x + 6, y + 5, amount == 1 ? "" : amount)
         }
     }
 
     Events.on(self, "gui_overlay_render", function() {
         var startX = config.x;
         var startY = config.y;
-        drawArmor(3, startX, startY)
-        drawArmor(2, startX, startY + 16)
-        drawArmor(1, startX, startY + 32)
-        drawArmor(0, startX, startY + 48)
+        drawArmor(39, startX, startY)
+        drawArmor(38, startX, startY + 16)
+        drawArmor(37, startX, startY + 32)
+        drawArmor(36, startX, startY + 48)
     })
 
     Events.on(self, 'game_loop', function () {
